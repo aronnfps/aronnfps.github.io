ae06f10c0ae7a93e6876b8a2ce0ad74254a1f346
aronnfps <gfbbestee1907@gmail.com>
2025-02-05 15:34:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-window.onload = function() {
-    const terminal = document.querySelector('.terminal');
-    terminal.style.display = 'block'; // Sayfa yüklendiğinde terminali göster
-}
-window.addEventListener('scroll', function() {
-    const elements = document.querySelectorAll('.fade-in-on-scroll');
-    elements.forEach(function(element) {
-        if (element.getBoundingClientRect().top < window.innerHeight) {
-            element.classList.add('visible');
-        }
-    });
-});
 // Hacker-style yazı eklemek için script
 document.addEventListener("DOMContentLoaded", function() {
     const hackerText = document.querySelector(".hacker-text");
