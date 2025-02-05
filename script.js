af83bf2b21eacf782488873aeb891a8b7d379f57
aronnfps <gfbbestee1907@gmail.com>
2025-02-05 15:19:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ window.onload = function() {
     const terminal = document.querySelector('.terminal');
     terminal.style.display = 'block'; // Sayfa yüklendiğinde terminali göster
 }
+window.addEventListener('scroll', function() {
+    const elements = document.querySelectorAll('.fade-in-on-scroll');
+    elements.forEach(function(element) {
+        if (element.getBoundingClientRect().top < window.innerHeight) {
+            element.classList.add('visible');
+        }
+    });
+});
